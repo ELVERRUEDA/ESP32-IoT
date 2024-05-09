871a9b6a5e096b8d6ee475f1ba6c4b5233f67f4b
Elver Rueda <112958858+ELVERRUEDA@users.noreply.github.com>
2024-05-08 23:41:27 -0500

    V1.1
    Conexion blynk para manejo desde dashboard del dele y visualizacion de metricas de temperatura y humedad

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 
+//Define el ID y nombre de la plantilla Blynk
+#define BLYNK_TEMPLATE_ID "TMPL2wcKOfKHp"
+#define BLYNK_TEMPLATE_NAME "Dht11"
+#define BLYNK_AUTH_TOKEN "-pCgcQcxYonLH7RexMsHtH16yCDS0n3x"
+
 #include <Arduino.h>
 #include <DHT.h>
 #include <Wire.h> 
@@ -9,23 +14,27 @@
 #include <WebServer.h>
 #include <DNSServer.h>
 #include <PubSubClient.h>
+#include <BlynkSimpleEsp32.h>
+
 
 #define DHTPIN 14
 #define DHTTYPE DHT11
 #define RELAY_PIN 26
 
-const char* ssid = "no vives de ensalada";
-const char* pass = "sisas420";
-const char* mqtt_server = "40.85.182.68";
+const char* ssid = "Red_IoT";
+const char* pass = "Rapsoda25";
+const char* mqtt_server = "192.168.1.71";
 int mqtt_port = 1883;
 const char* mqtt_topic_temp = "esp32/dht11/temperature";
 const char* mqtt_topic_hum = "esp32/dht11/humidity";
 const char* mqtt_topic_relay = "esp32/relay";
+char auth[] = "-pCgcQcxYonLH7RexMsHtH16yCDS0n3x";
 
 DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 WiFiClient espClient;
 PubSubClient client(espClient);
+
 
 bool relayState = false; // Variable para mantener el estado del relé
 
@@ -42,6 +51,18 @@ void updateLCD() {
   lcd.setCursor(10, 1);
   lcd.print("R: ");
   lcd.print(relayState ? "ON" : "OFF");
+}
+
+BLYNK_WRITE(V1) {
+  int value = param.asInt(); // Lee el estado del botón (1 o 0)
+  if (value == 1) {
+    digitalWrite(RELAY_PIN, LOW); // Activa el relé
+    relayState = true;
+  } else {
+    digitalWrite(RELAY_PIN, HIGH); // Desactiva el relé
+    relayState = false;
+  }
+  updateLCD(); // Actualiza la pantalla LCD
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -65,7 +86,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
     }
   }
 }
-
 
 void reconnect() {
   while (!client.connected()) {
@@ -99,11 +119,16 @@ void setup() {
 
   lcd.init();
   lcd.backlight();
+  Blynk.connect();
+  Blynk.config(auth);
+  Blynk.begin(auth, ssid, pass);
   pinMode(RELAY_PIN, OUTPUT);
   updateLCD();
 }
 
 void loop() {
+
+  Blynk.run();
   if (!client.connected()) {
     reconnect();
   }
@@ -114,7 +139,12 @@ void loop() {
 
   if (isnan(temperature) || isnan(humidity)) {
     Serial.println("Error al leer el sensor DHT!");
+    Blynk.virtualWrite(V0, String(temperature, 1));
+    Blynk.virtualWrite(V2, String(humidity, 1));
     return;
+  }else {
+    Blynk.virtualWrite(V0, "NAN");
+    Blynk.virtualWrite(V2, "NAN");
   }
 
   Serial.print("Temperatura: ");
@@ -132,7 +162,6 @@ void loop() {
   dtostrf(humidity, 4, 2, humString);
   client.publish(mqtt_topic_temp, tempString);
   client.publish(mqtt_topic_hum, humString);
-
   lcd.clear();
 
   lcd.setCursor(0, 0);
